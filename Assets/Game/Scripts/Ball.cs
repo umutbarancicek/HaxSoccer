8925994331338de28e765fdb6067cab115fc3d28
Orkun mert <58224943+mertyjsx@users.noreply.github.com>
2023-03-20 22:47:47 +0100

    feature: rolling ball

--- Assets/Game/Scripts/Ball.cs
+++ Assets/Game/Scripts/Ball.cs
@@ -6,6 +6,9 @@ public class Ball : MonoBehaviour
 {
     
 private bool stickToPlayer;
+private float speed;
+private Vector3 previousLocation;
+
 [SerializeField] private Transform transformPlayer;
 [SerializeField] private Transform playerBallPosition;
 
@@ -23,13 +26,15 @@ private bool stickToPlayer;
 
        float distanceToPlayer = Vector3.Distance(transformPlayer.position, transform.position);
         if(distanceToPlayer < 0.5){
-
             stickToPlayer = true;
-
         }
     }
     else {
+        Vector2 currentLocation = new Vector2(transform.position.x,transform.position.y);
+        speed = Vector2.Distance(currentLocation,previousLocation) / Time.deltaTime;
         transform.position = playerBallPosition.position;
+        transform.Rotate(new Vector3(transformPlayer.right.x,0,transformPlayer.right.z),speed,Space.World);
+        previousLocation=currentLocation;
     }
 
         
