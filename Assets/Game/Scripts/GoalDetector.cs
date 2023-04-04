d1e8fa6df35d10d9958d59792b68f778434be531
Orkun mert <58224943+mertyjsx@users.noreply.github.com>
2023-04-05 01:11:23 +0200

    respawn is done, shooting power feature added ,slider added

--- Assets/Game/Scripts/GoalDetector.cs
+++ Assets/Game/Scripts/GoalDetector.cs
@@ -1,46 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class GoalDetector : MonoBehaviour
 {
     [SerializeField] private Player scriptPlayer;
     [SerializeField] private GameObject goalText;
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject ball;
+    [SerializeField] private Transform ballStartLocation;
+    [SerializeField] private Transform playerStartLocation;
 
+    void Start()
+    {
+        goalText.SetActive(false);
+    }
+    IEnumerator ToggleShowGoalText(int second)
+    {
+        goalText.SetActive(true);
+        yield return new WaitForSeconds(second);
+        goalText.SetActive(false);
 
-  void Start(){
-    goalText.SetActive(false);
-  }
-
-
-
-   IEnumerator ToggleShowGoalText(int second){
-    goalText.SetActive(true);
-   yield return new WaitForSeconds(second);
-   goalText.SetActive(false);
-  
- }
-
+    }
     public void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<Ball>() != null)
         {
-
-
-     
             if (name.Equals("GoalDetector1"))
             {
                 scriptPlayer.IncreaseMyScore();
-              
+
             }
             else
             {
                 scriptPlayer.IncreaseOtherScore();
 
             }
-                 StartCoroutine(ToggleShowGoalText(2));
-
-            
+            StartCoroutine(ToggleShowGoalText(2));
+            player.transform.DOMove(playerStartLocation.position,0.1f);
+            ball.transform.DOMove(ballStartLocation.position,0.1f);
+            Rigidbody rb = ball.GetComponent<Rigidbody>();
+            rb.velocity = rb.velocity.normalized * 0f;
         }
     }
 }
--- Assets/Game/Scripts/Player.cs
+++ Assets/Game/Scripts/Player.cs
@@ -12,6 +12,8 @@ public class Player : MonoBehaviour
     private Animator animator;
     private CharacterController characterController;
     private CharacterController controller;
+    [SerializeField] private Slider slider;
+    [SerializeField] private Transform camera;
     #endregion
 
     #region Score
@@ -22,40 +24,76 @@ public class Player : MonoBehaviour
     #endregion
 
     #region Audio Sources
-     [SerializeField] private AudioSource soundCheer;
-     [SerializeField] private AudioSource soundKick;
+    [SerializeField] private AudioSource soundCheer;
+    [SerializeField] private AudioSource soundKick;
     #endregion
 
     #region Ball
     private Ball ballAttachedToPlayer;
     public Ball BallAttachedToPlayer { get => ballAttachedToPlayer; set => ballAttachedToPlayer = value; }
     #endregion
-    
+
     #region Variables
     [SerializeField] private float shootPower;
     private float timeShot;
     private const int LAYER_SHOOT = 1;
     private float distanceSinceLastDribble;
- 
+
+    private float shootButtonDownTime = 0;
+    private float shootHoldDuration = 0;
+    private float additionalTime = 0;
+    private float maxHoldTime = 3f;
+
+    [SerializeField] private float shootMultiplier = 2f;
+
+
     #endregion
     // Start is called before the first frame update
     void Start()
     {
-      
+
         characterController = GetComponent<CharacterController>();
         starterAssetsInputs = GetComponent<StarterAssetsInputs>();
         animator = GetComponent<Animator>();
         controller = GetComponent<CharacterController>();
-
     }
 
+
+    private void LateUpdate()
+    {
+        slider.transform.LookAt(camera.position);
+    }
     // Update is called once per frame
     void Update()
     {
-        
         float speed = new Vector3(characterController.velocity.x, 0, characterController.velocity.z).magnitude;
-        if (starterAssetsInputs.shoot)
+
+        if (Input.GetKeyDown(KeyCode.Return))
         {
+            shootButtonDownTime = Time.time;
+        }
+
+        if (Input.GetKey(KeyCode.Return))
+        {
+            // Calculate how long the button has been held down
+            shootHoldDuration = Time.time - shootButtonDownTime;
+
+            slider.value = (float)shootHoldDuration / maxHoldTime;
+
+            if (shootHoldDuration >= maxHoldTime)
+            {
+                // Release the button to simulate player releasing it
+                Input.GetKeyUp(KeyCode.Return);
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.Return))
+        {
+            slider.value = 0f;
+            shootHoldDuration = Mathf.Min(shootHoldDuration, maxHoldTime); // Clamp the hold duration to the max hold time
+            additionalTime = shootHoldDuration;
+            Debug.Log(shootHoldDuration.ToString("F2"));
+
             starterAssetsInputs.shoot = false;
             timeShot = Time.time;
             animator.Play("Shoot", LAYER_SHOOT, 0f);
@@ -64,21 +102,20 @@ public class Player : MonoBehaviour
         if (timeShot > 0)
         {
             // shoot ball
-            if( ballAttachedToPlayer != null && Time.time - timeShot > 0.2)
+            if (ballAttachedToPlayer != null && Time.time - timeShot > 0.2)
             {
-                
                 soundKick.Play();
                 ballAttachedToPlayer.StickToPlayer = false;
 
                 Rigidbody rigidbody = ballAttachedToPlayer.transform.gameObject.GetComponent<Rigidbody>();
                 Vector3 shootdirection = transform.forward;
                 shootdirection.y += 0.3f;
-                rigidbody.AddForce(shootdirection * shootPower, ForceMode.Impulse);
+                Debug.Log(additionalTime);
+                rigidbody.AddForce(shootdirection * shootPower * shootMultiplier * additionalTime, ForceMode.Impulse);
                 ballAttachedToPlayer = null;
             }
-
             // finished kicking animation
-            if(Time.time - timeShot > 0.5)
+            if (Time.time - timeShot > 0.5)
             {
                 timeShot = 0;
             }
@@ -88,25 +125,25 @@ public class Player : MonoBehaviour
             animator.SetLayerWeight(LAYER_SHOOT, Mathf.Lerp(animator.GetLayerWeight(LAYER_SHOOT), 0f, Time.deltaTime * 10f));
         }
 
-        if (goalTextColorAlpha>0)
+        if (goalTextColorAlpha > 0)
         {
             goalTextColorAlpha -= Time.deltaTime;
             // textGoal.alpha = goalTextColorAlpha;
             //textGoal.fontSize = 200 - (goalTextColorAlpha * 120);
         }
 
-        if (ballAttachedToPlayer!=null)
+        if (ballAttachedToPlayer != null)
         {
             distanceSinceLastDribble += speed * Time.deltaTime;
             if (distanceSinceLastDribble > 3)
             {
-           
+
                 distanceSinceLastDribble = 0;
             }
         }
     }
 
-   public void IncreaseMyScore()
+    public void IncreaseMyScore()
     {
         myScore++;
         UpdateScore();
@@ -123,5 +160,5 @@ public class Player : MonoBehaviour
         textScore.text = "Score: " + myScore + "-" + otherScore;
     }
 
-   
+
 }
